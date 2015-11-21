16415b129cc6f5a41c1a7532e0a443be192a986d
owen <smellyday@gmail.com>
2015-11-21 15:46:42 +0800

    Demo

---
+++ CDemo/HelloWorld.c
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.c
+//  CDemo
+//
+//  Created by owen on 11/21/15.
+//
+//
+
+#include "HelloWorld.h"
+
+int main() {
+    printf("Hello World"" !\n");
+    return 0;
+}
---
+++ CDemo/HelloWorld.h
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.h
+//  CDemo
+//
+//  Created by owen on 11/21/15.
+//
+//
+
+#ifndef HelloWorld_h
+#define HelloWorld_h
+
+#include <stdio.h>
+
+#endif /* HelloWorld_h */
