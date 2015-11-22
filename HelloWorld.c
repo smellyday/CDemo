e7ff6970b95abd0b9571d874235256c0752b572b
owen <smellyday@gmail.com>
2015-11-22 23:06:05 +0800

    little

--- HelloWorld.c
+++ HelloWorld.c
@@ -8,7 +8,19 @@
 
 #include "HelloWorld.h"
 
+int accum = 6;
+int sum(int x, int y);
+
 int main() {
+    int x = 8;
+    int y = 16;
+    accum = sum(x, y);
     printf("Hello World"" !\n");
     return 0;
+}
+
+int sum(int x, int y) {
+    int t = x + y;
+    accum += t;
+    return t;
 }
